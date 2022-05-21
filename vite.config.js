5dae715d131e872605ff2b0f55febfec9cbee2d8
Kellojo <kellojo1@gmail.com>
2022-05-21 15:04:08 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
-  build: {
-    target: 'esnext',
-    polyfillDynamicImport: false,
-  },
+    base: './',
+    plugins: [solidPlugin()],
+    build: {
+        target: 'esnext',
+        polyfillDynamicImport: false,
+    },
 });
