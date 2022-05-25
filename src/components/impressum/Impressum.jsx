9c0df6d52cae95ae23fe9956a57179befc5d72aa
Kellojo <kellojo1@gmail.com>
2022-05-25 20:32:06 +0200

    Added impressum

--- src/components/impressum/Impressum.jsx
+++ src/components/impressum/Impressum.jsx
@@ -1,9 +1,24 @@
 import styles from './Impressum.module.css';
+import sharedStyles from '../shared.module.css';
 
 function Impressum(props) {
     return (
         <div>
-            The impressum
+            <div class={styles.section}>
+                <div class={`${sharedStyles.title} ${styles.title}`}>Impressum</div>
+                <span class={styles.subtitle}>Daniel Fritz</span>
+                <span class={styles.subtitle}>Waldparkstr. 26</span>
+                <span class={styles.subtitle}>68163 Mannheim</span>
+                <span class={styles.subtitle}>Germany</span>
+            </div>
+
+            <div class={styles.section}>
+                <div class={`${sharedStyles.title} ${styles.title}`}>Contact</div>
+                <div>
+                    <a href="mailto:contact.kellojo@gmail.com">contact.kellojo@gmail.com</a>
+                </div>
+            </div>
+            
         </div>
         
     );
