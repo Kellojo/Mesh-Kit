1960cc0187786feb19798f390202ee6b5737d163
Kellojo <kellojo1@gmail.com>
2022-06-11 17:22:58 +0200

    added basic test setup

---
+++ cypress/e2e/library.cy.js
@@ -0,0 +1,15 @@
+describe('empty spec', () => {
+  it('passes', () => {
+      cy.visit('/');
+
+      cy.get('a[href="#/library"]').click();
+  });
+
+    it('passes', () => {
+        cy.visit('/');
+
+        cy.get('button').contains('.blend').click();
+    });
+
+
+})
