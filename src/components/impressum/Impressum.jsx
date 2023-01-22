881c0a1b1ef3a2a12e5d5ee39ff0cf015367c69b
Kellojo <kellojo1@gmail.com>
2023-01-22 13:56:51 +0100

    Update Impressum.jsx

--- src/components/impressum/Impressum.jsx
+++ src/components/impressum/Impressum.jsx
@@ -4,14 +4,7 @@ import sharedStyles from '../shared.module.css';
 function Impressum(props) {
     return (
         <div>
-            <div class={styles.section}>
-                <div class={`${sharedStyles.title} ${styles.title}`}>Impressum</div>
-                <span class={styles.subtitle}>Daniel Fritz</span>
-                <span class={styles.subtitle}>Waldparkstr. 26</span>
-                <span class={styles.subtitle}>68163 Mannheim</span>
-                <span class={styles.subtitle}>Germany</span>
-            </div>
-
+        
             <div class={styles.section}>
                 <div class={`${sharedStyles.title} ${styles.title}`}>Contact</div>
                 <div>
